e911761cc5e5ccf259d531b940cde82fdbb07ddc
matetsiko <tsiko.mate@gmail.com>
2023-05-28 15:30:27 +0400

    davaleba

---
+++ task2.lua
@@ -0,0 +1,31 @@
+a = 1
+b = 2
+c = 3
+d = 4
+e =5
+f =6
+g =7
+h =8
+i =9
+j =10
+k =11
+l =12
+m =13
+n =14
+o =15
+p =16
+q =17
+r =18
+s =19
+t =20
+v =21
+w =22
+x =23
+y =24
+z =25
+print(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, v, w, x, y, z)
+
+
+for a = 1, 100 do
+    print( "               " .. a)
+end
